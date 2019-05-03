9fa8a42bb6bff194a12af5bbf335cdd79835a8e9
Kevin Griffin <me@kevin.jp>
2019-05-03 11:43:46 +0000

    Fixes install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 hostname=$(hostname -s)
-if [ -f "$hostname"/authorized_keys ]
+if [ -f hosts/"$hostname"/authorized_keys ]
 then
-  cp "$hostname"/authorized_keys "$HOME"/.ssh/authorized_keys
+  cp hosts/"$hostname"/authorized_keys "$HOME"/.ssh/authorized_keys
 else
   cp authorized_keys "$HOME"/.ssh/authorized_keys
 fi
