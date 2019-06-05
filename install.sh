80904c933dfdfd31adb560d6db73512b0332eb0a
Kevin Griffin <me@kevin.jp>
2019-06-05 17:38:56 +0900

    Removes unused install script.

--- install.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-hostname=$(hostname -s)
-if [ -f hosts/"$hostname"/authorized_keys ]
-then
-  cp hosts/"$hostname"/authorized_keys "$HOME"/.ssh/authorized_keys
-else
-  cp authorized_keys "$HOME"/.ssh/authorized_keys
-fi
