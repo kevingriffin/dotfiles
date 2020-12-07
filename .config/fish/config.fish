e63e9f3c8f979baac0ae729d44361252c4b38751
Kevin Griffin <me@kevin.jp>
2020-12-07 13:19:02 +0900

    fish: no more workaround for git on arm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,12 +5,6 @@ set -x PAGER less -R
 set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
 
-# Hack until I get git working on
-# Apple silicon
-if test (uname -p) = "arm"
-  set -g theme_display_git no
-end
-
 set -g theme_display_vagrant no
 set -g theme_display_docker_machine no
 set -g theme_display_k8s_context yes
