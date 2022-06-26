45ada40a394951e00a9187aedf9c2ee173b002e9
Kevin Griffin <me@kevin.jp>
2022-06-26 23:51:23 +0900

    fish: removed unused stuff The settings were from a theme no longer in use.

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -5,44 +5,12 @@ set -x PAGER less -R
 set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
 
-set -g theme_display_vagrant no
-set -g theme_display_docker_machine no
-set -g theme_display_k8s_context yes
-set -g theme_display_hg no
-set -g theme_display_virtualenv no
-set -g theme_display_nix yes
-set -g theme_display_ruby no
-set -g theme_display_nvm no
-set -g theme_display_user no
-set -g theme_display_hostname ssh
-set -g theme_display_vi no
-set -g theme_display_date no
-set -g theme_display_cmd_duration no
-set -g theme_title_display_process yes
-set -g theme_title_display_path no
-set -g theme_title_display_user yes
-set -g theme_title_use_abbreviated_path no
-set -g theme_date_format "+%a %H:%M"
-set -g theme_date_timezone Asia/Tokyo
-set -g theme_avoid_ambiguous_glyphs yes
-set -g theme_powerline_fonts no
-set -g theme_nerd_fonts yes
-set -g theme_show_exit_status yes
-set -g theme_display_jobs_verbose yes
-set -g theme_color_scheme gruvbox
-set -g fish_prompt_pwd_dir_length 0
-set -g theme_project_dir_length 1
-set -g theme_newline_cursor yes
-set -g theme_newline_prompt ''
-set -g theme_display_sudo_user yes
-
 abbr -a be bundle exec
 abbr -a g git
 abbr -a gf git ls-files --modified --others --exclude-standard
 abbr -a v nvim
 abbr -a ddl cd $HOME/Downloads
 abbr -a icl cd "$HOME/Library/Mobile\ Documents/com~apple~CloudDocs"
-abbr -a tmp cd "$HOME/tmp"
 abbr -a src cd "$HOME/src"
 abbr -a dot cd "$HOME/dotfiles"
 abbr -a dc  cd "$HOME/dotfiles/config/.config"
@@ -50,8 +18,6 @@ abbr -a ik cd "$HOME/src/iknow"
 abbr -a ec cd "$HOME/src/eikaiwa_content"
 abbr -a ecf cd "$HOME/src/eikaiwa_content_frontend"
 abbr -a ecr cd "$HOME/src/eikaiwa-realtime"
-abbr -a cfiles "(git ls-files --modified --exclude-standard | fzf -m)"
-abbr -a afiles "(git ls-files --others --exclude-standard | fzf -m)"
 abbr -a e "exa"
 abbr -a l "exa -l --no-permissions --octal-permissions"
 abbr -a ll "exa -a -l --no-permissions --octal-permissions"
