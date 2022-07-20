032d985eb00d100752bdb129a2e83eface1b5e32
Kevin Griffin <me@kevin.jp>
2022-07-20 20:23:13 +0900

    neovim: updates for 0.7.2

--- config/.config/nvim/lua/theme.lua
+++ config/.config/nvim/lua/theme.lua
@@ -1,12 +1,11 @@
 vim.o.background = "dark"
--- require('material.functions').change_style("darker")
-vim.g.material_style = "darker"
--- vim.cmd('colorscheme material')
+
 local c = require("gruvbox-baby.colors").config()
+
 vim.g.gruvbox_baby_highlights = {
-  MatchParen = {fg = c.orange, style = "bold,underline" },
+  MatchParen = {fg = c.orange, style = "bold" },
   StatusLine = {fg = c.background, style = "bold" },
   Identifier = {fg = c.pink}
-  -- StatusLineNC = {fg = c.background, style = "bold" }
 }
+
 vim.cmd('colorscheme gruvbox-baby')
