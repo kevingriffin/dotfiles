82366a285fe5de597329a8ff5f1ed1d6af1885d3
Kevin Griffin <me@kevin.jp>
2023-12-21 16:08:26 +0900

    fish: helix

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -1,5 +1,3 @@
-set -x GIT_EDITOR nvim
-set -x EDITOR nvim
 set -x PAGER less -R
 
 set -x LC_ALL en_US.UTF-8
@@ -8,7 +6,7 @@ set -x LC_CTYPE en_US.UTF-8
 abbr -a be bundle exec
 abbr -a g git
 abbr -a gf git ls-files --modified --others --exclude-standard
-abbr -a v nvim
+abbr -a h hx
 abbr -a ddl cd $HOME/Downloads
 abbr -a icl cd "$HOME/Library/Mobile\ Documents/com~apple~CloudDocs"
 abbr -a src cd "$HOME/src"
@@ -20,7 +18,8 @@ abbr -a ecf cd "$HOME/src/eikaiwa_content_frontend"
 abbr -a ecr cd "$HOME/src/eikaiwa-realtime"
 abbr -a e cd "$HOME/Developer/Eikaiwa"
 abbr -a l "eza -l --no-permissions --octal-permissions"
-abbr -a ll "eza -a -l --no-permissions --octal-permissions"
+abbr -a ll "eza -l --no-permissions --octal-permissions"
+abbr -a la "eza -a -l --no-permissions --octal-permissions"
 abbr -a ea "eza -a"
 abbr -a er "eza -R -L2"
 abbr -a era "eza -R -a -L2"
